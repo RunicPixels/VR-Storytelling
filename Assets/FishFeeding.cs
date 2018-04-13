f38d65e401c651f69e1c0ddaedd489ed27558994
Cheezegami <superronald@live.nl>
2018-04-13 11:09:03 +0200

    updeet

---
+++ Assets/FishFeeding.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishFeeding : MonoBehaviour {
+    private bool isFishFed = false;
+    private AudioSource src;
+
+    public AudioClip succeedSound;
+
+	// Use this for initialization
+	void Start () {
+		if(GetComponent<AudioSource>() != null) {
+            src = GetComponent<AudioSource>();
+        }
+        else {
+            src = gameObject.AddComponent<AudioSource>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Vissenvoer" && !isFishFed) {
+            isFishFed = true;
+            src.PlayOneShot(succeedSound);
+        }
+    }
+}
